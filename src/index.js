c8b5cd3852c79a8da53f3b28912c46542bef0277
Minaduki Shigure <i@minaduki.cn>
2024-06-14 18:08:44 +0800

    Modified domains.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.minaduki.dev": dockerHub,
+  "quay.minaduki.dev": "https://quay.io",
+  "gcr.minaduki.dev": "https://gcr.io",
+  "k8s-gcr.minaduki.dev": "https://k8s.gcr.io",
+  "k8s.minaduki.dev": "https://registry.k8s.io",
+  "ghcr.minaduki.dev": "https://ghcr.io",
+  "cloudsmith.minaduki.dev": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.minaduki.dev": dockerHub,
 };
 
 function routeByHosts(host) {
